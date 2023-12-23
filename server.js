30fbc6437b80b4aab425365e5837578d022ca99d
ANKIT KASHYAP <60142710+ANKIT-KASHYAP@users.noreply.github.com>
2023-12-23 12:29:19 +0530

    URL Update in server.js

--- server.js
+++ server.js
@@ -41,7 +41,8 @@ const options ={
     
         },
         servers:[{
-            url:"http://localhost:8080"
+            // url:"http://localhost:8080"
+            url:"https://nodejs-job-portal-application.onrender.com",
         }
     ]
     },
